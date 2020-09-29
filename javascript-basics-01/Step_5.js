5a14b101f519d2ce196b001eebf145bac9ee0970
hamidadribika <hamida.dr02@yahoo.com>
2020-09-29 21:12:52 +0300

    Step5

---
+++ javascript-basics-01/Step_5.js
@@ -0,0 +1,9 @@
+var n1 =document.getElementById("first_number").value;
+var n2 =document.getElementById("second_number").value;
+
+n1=parseInt(n1);
+n2=parseInt(n2);
+
+var product=parseInt(n1*n2);
+
+alert("the product of the two numbers is : "+ product);
