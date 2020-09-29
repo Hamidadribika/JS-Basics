c42b255a3b49e87cfd6412f38b3fe974995b358b
hamidadribika <hamida.dr02@yahoo.com>
2020-09-29 11:20:04 +0300

    Step4

---
+++ javascript-basics-01/Step_4.js
@@ -0,0 +1,6 @@
+
+var name=prompt("Please enter your name : ");
+var surname=prompt("Please enter your surname : ");
+var city=prompt("Please enter your city : ");
+
+alert("Nom : "+name +"\n Prénom: "+ surname + "\n Ville: "+city)
